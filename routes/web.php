76e363e5e5cbf83ce5d168723c5a01fe87b16fbd
d3ric3 <d3ric3@gmail.com>
2024-10-01 12:42:35 +0800

    Final EP 3:43

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisterUserController;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/login', [SessionController::class, 'destroy']);
