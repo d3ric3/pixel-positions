c829a13d6d3aa8125b595605be9b74bc46011055
d3ric3 <d3ric3@gmail.com>
2024-11-02 13:05:16 +0800

    Added ASSET_URL env to fix img issue

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,5 +1,5 @@
 @props(['employer', 'width' => 90])
 
-<img src="{{ asset('storage/' . $employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
 
 {{-- <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}/{{ $width }}" alt=""> --}}
